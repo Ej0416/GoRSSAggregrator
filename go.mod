64682b49314fea8642209edd041877761b89cdb5
Ej <elnesjani@gmail.com>
2025-11-10 20:52:12 +0800

    setup server init

--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module github.com/Ej0416/GoRSSAggregrator
 go 1.25.0
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.2.3
+
+require github.com/go-chi/cors v1.2.2
--- main.go
+++ main.go
@@ -3,18 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Application started")
 	godotenv.Load()
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT not fund in environment variables")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Println("Server is starting on port:", port)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("Listening on port:", port)
